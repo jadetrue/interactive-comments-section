61a2854a4da5d2e823ce2c7b2eb4bf77a17e6eec
Jade True <jadetruexo@gmail.com>
2024-03-18 16:23:03 +0000

    Add styles to textarea

--- src/components/AddNewComment.tsx
+++ src/components/AddNewComment.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Button } from "./Button";
 
 export const AddNewComment = ({ userImg, comment, onClick }) => {
+  const placeholder = "Add a comment..."
+
   return (
-    <div className="container">
-      <img src={userImg} alt="" width="30" height="30" />
-      <textarea>{comment}</textarea>
-      <Button name="Send" type="default" onClick={onClick} />
-    </div>
+    <div className="new-comment-container">
+      <div>
+        <textarea placeholder={placeholder}>{comment}</textarea>
+      </div>
+      <div className="">
+        <img src={userImg} alt="" width="30" height="30" />
+        <Button name="Send" type="default" onClick={onClick} />
+      </div>
+    </div >
   );
 };
