405bb58d550969e1e7074ee7e5ae430eb1ddcdee
Jade True <jadetruexo@gmail.com>
2024-03-19 12:47:10 +0000

    add style for new comment section using grid

--- src/components/AddNewComment.tsx
+++ src/components/AddNewComment.tsx
@@ -6,11 +6,9 @@ export const AddNewComment = ({ userImg, comment, onClick }) => {
 
   return (
     <div className="new-comment-container">
-      <div>
-        <textarea placeholder={placeholder}>{comment}</textarea>
-      </div>
-      <div className="">
-        <img src={userImg} alt="" width="30" height="30" />
+      <img className="author-img" src={userImg} alt="" width="30" height="30" />
+      <textarea className="new-comment" placeholder={placeholder}>{comment}</textarea>
+      <div className="reply-btn">
         <Button name="Send" type="default" onClick={onClick} />
       </div>
     </div >
