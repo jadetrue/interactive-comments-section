a4b9ec314f0ac3e7623423244660991f45362202
Jade True <jadetruexo@gmail.com>
2024-04-22 14:30:14 +0100

    disable button from adding more score

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -7,20 +7,23 @@ interface Score {
 
 const Score: React.FC<Score> = ({ score }) => {
   const [count, setCount] = useState(score);
+  const [isDisabled, setIsDisabled] = useState(false)
 
   const increase = () => {
     setCount((count) => count + 1);
+    setIsDisabled(true)
   };
 
   const decrease = () => {
     if (count > 0) setCount((count) => Math.max(score, count - 1));
+    setIsDisabled(false)
   };
 
   return (
     <div className="flex md:flex-col items-center w-fit h-fit md:self-center rounded-lg bg-light-200 font-medium">
-      <Button name="+" onClick={async () => increase()} />
+      <Button name="+" onClick={async () => increase()} disabled={isDisabled} />
       <p className="text-primary-400 font-medium">{count}</p>
-      <Button name="-" onClick={async () => decrease()} />
+      <Button name="-" onClick={async () => decrease()} disabled={!isDisabled} />
     </div>
   );
 };
